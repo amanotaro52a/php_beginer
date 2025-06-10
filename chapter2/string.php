de157a67d2dbd8cfde22f99a38282d3ac7fcd0e8
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-06-10 23:41:38 +0900

    四則演算,文字列,変数

---
+++ chapter2/string.php
@@ -0,0 +1,6 @@
+<?php
+$name = "PHP";
+echo "hello, $name" . PHP_EOL;
+echo 'Hello' . PHP_EOL;
+echo 'Hello' . 'Laravel' . PHP_EOL;
+echo 'Hello' . ' ' . 'Laravel' . PHP_EOL;
---
+++ chapter2/string_variable.php
@@ -0,0 +1,6 @@
+<?php
+$x = 'This is a';
+$y = 'pen';
+echo $x . ' ' . $y . PHP_EOL;
+echo "$x $y" . PHP_EOL;
+echo '$x $y' . PHP_EOL;
