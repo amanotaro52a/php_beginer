1015e9e62e9564f0302709b1c5eb59e6026af04c
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-06-13 23:08:04 +0900

    演算子,条件分岐,繰り返し処理

---
+++ chapter3/comparison_operators.php
@@ -0,0 +1,10 @@
+<?php
+$a = 20;
+$b = 40;
+
+echo var_export($a == $b, true) . PHP_EOL;
+echo var_export($a != $b, true) . PHP_EOL;
+echo var_export($a > $b, true) . PHP_EOL;
+echo var_export($a < $b, true) . PHP_EOL;
+echo var_export($a >= $b, true) . PHP_EOL;
+echo var_export($a <= $b, true) . PHP_EOL;
---
+++ chapter3/each_loop.php
@@ -0,0 +1,4 @@
+<?php
+foreach (range(0,6) as $a) {
+    echo ($a + 1) . "回目\n";
+}
