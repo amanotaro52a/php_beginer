7704f912651cc51c3f2d42c812b8648e8a72ffdb
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-06-17 16:54:48 +0900

    クラス

---
+++ chapter7/method_importance.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello, Alex!\n";
+echo "Hello, David\n";
+echo "Hello, Mariah\n";
+
+function greet($name) {
+    echo "Hello, $name!\n";
+}
+
+greet("Alex");
+greet("David");
+greet("Mariah");
